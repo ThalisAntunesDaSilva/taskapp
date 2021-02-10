de1e5858bb2eb82219ed4d92f7b7da4a911b848c
Thalis Silva <thalis.silva@zallpy.com>
2021-02-10 13:33:01 -0300

    atualizacao projeto

--- TaskApp/src/main/java/br/com/thalis/PessoaBean.java
+++ TaskApp/src/main/java/br/com/thalis/PessoaBean.java
@@ -1,27 +1,53 @@
 package br.com.thalis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 
 	private String nome;
-	private String sobrenome;
+	private List<String> nomes = new ArrayList<String>();
+	private HtmlCommandButton commandButton;
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+
+	public String addNome() {
+		nomes.add(nome);
+
+		if (nomes.size() > 0) {
+			commandButton.setDisabled(true);
+		}
+
+		return " ";
+	}
 
 	public String getNome() {
 		return nome;
 	}
 
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
 	}
 
 }
---
+++ TaskApp/src/test/java/TesteJPA.java
@@ -0,0 +1,18 @@
+
+
+
+
+import javax.persistence.Persistence;
+
+
+
+public class TesteJPA {
+
+	public static void main(String[]args) {
+	
+	 Persistence.createEntityManagerFactory("TaskApp"); 
+		
+	
+	}
+	
+}
